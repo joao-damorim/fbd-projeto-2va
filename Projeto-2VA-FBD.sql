5bd176878370c46692d5d5eacfa7eb98f7eb596e
João D'Amorim <joao.damorim@gmail.com>
2018-02-17 16:07:52 -0300

    Script para criação do banco de dados

---
+++ Projeto-2VA-FBD.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE Projeto;
+
+USE Projeto;
+
+CREATE TABLE Departamento(
+	CodDept INT  NOT NULL,
+    Nome VARCHAR(100),
+    Gerente VARCHAR(11),
+    CONSTRAINT PRIMARY KEY (CodDept),
+    CONSTRAINT FOREIGN KEY (Gerente) REFERENCES Funcionario (CPF)
+    );
+    
+CREATE TABLE Funcionario(
+	CPF CHAR(11) NOT NULL,
+    Nome VARCHAR(100),
+    Telefone VARCHAR(20),
+    Salario FLOAT,
+    Tipo
+    CodDept INT NOT NULL,
+    CONSTRAINT PRIMARY KEY (CPF),
+    CONSTRAINT FOREIGN KEY (CodDept) REFERENCES Departamento (CodDept)
+    );
+
+CREATE TABLE FuncionarioADM(
+	CPF CHAR(11) NOT NULL,
+    HorarioInicio TIME,
+    HorarioSaida TIME, 
+    NumSala INT,
+    CONSTRAINT PRIMARY KEY (CPF),
+    CONSTRAINT FOREIGN KEY (CPF) REFERENCES Funcionario (CPF)
+    );
+
+CREATE TABLE FuncionarioProjeto(
+	CPF CHAR(11) NOT NULL,
+    CodProjeto INT NOT NULL,
+    CONSTRAINT PRIMARY KEY (CPF),
+    CONSTRAINT FOREIGN KEY (CPF) REFERENCES Funcionario (CPF),
+    CONSTRAINT FOREIGN KEY (CodDept) REFERENCES Departamento (CodDept)
+    );
+    
+CREATE TABLE Projeto(
+	Codigo INT NOT NULL,
+    Nome VARCHAR(100),
+    DataInicial DATE,
+    DataFinal DATE,
+    CONSTRAINT PRIMARY KEY (Codigo)
+    );
+    
+CREATE TABLE Historico_Funcionario(
+	CPF CHAR(11) NOT NULL,
+    Cargo VARCHAR(100),
+    DataInicial DATE,
+    DataFinal DATE,
+    CodDept INT NOT NULL,
+    CONSTRAINT PRIMARY KEY (CPF),
+    CONSTRAINT PRIMARY KEY (DataInicial),
+    CONSTRAINT FOREIGN KEY (CPF) REFERENCES Funcionario (CPF)
+    );
